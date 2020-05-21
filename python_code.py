3156930fbbaec0618ab3ee93752e5a42419207b1
varasavelli <65738619+varasavelli@users.noreply.github.com>
2020-05-21 18:48:37 -0400

    Add files via upload

--- python_code.py
+++ python_code.py
@@ -1,2 +1,5 @@
-print “Hello Github!”
+### This project will be used for capstone project
+import pandas as pd # primary data structure library
+import numpy as np  # useful for many scientific computing in Python
+print ("Hello Capstone Project Course!")
 
